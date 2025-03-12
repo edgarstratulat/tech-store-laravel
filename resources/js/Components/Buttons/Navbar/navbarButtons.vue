d4833e04156ecbe0806fc2f218240652b399a9df
Edgar Sousa <stratulatedgar@gmail.com>
2025-03-12 12:17:45 +0000

    feat: Created new component called navbarButtons for the navbar buttons in navbar

---
+++ resources/js/Components/Buttons/Navbar/navbarButtons.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="hidden md:flex md:items-center md:space-x-6 ml-8">
+        <div v-for="btn in filterButtonsNavBar" :key="btn.id">
+            <button
+                @click="
+                    btn.dropdown
+                        ? toggleDropdownNavbar(btn.id)
+                        : redirect(btn.route)
+                "
+                class="text-gray-500 hover:text-blue-600 transition duration-300"
+            >
+                {{ btn.button_name }}
+            </button>
+
+            <ul
+                class="absolute p-5 z-10 mt-2 w-52 rounded-lg border border-slate-200 bg-white shadow-lg text-center text-md"
+                v-if="btn.dropdown && isDropdownNavbarOpen(btn.id)"
+            >
+                <li
+                    class="cursor-pointer text-slate-800 hover:text-blue-600"
+                    v-for="option in JSON.parse(btn.dropdownOptions)"
+                    :key="option.id"
+                    @click="redirect(option.route)"
+                >
+                    {{ option.button_name }}
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isDropdownOpen: false,
+            dropdownNavbarStates: {},
+        };
+    },
+    props: {
+        Utilizador: {
+            type: Object,
+        },
+        Buttons: {
+            type: Array,
+        },
+    },
+    computed: {
+        filterButtonsNavBar() {
+            const navbarButtons = [1, 2, 3, 4, 5, 6];
+            return this.Buttons.filter((btn) => navbarButtons.includes(btn.id));
+        },
+        filterButtonsDropDownMenuNotLogged() {
+            const DropdownButtons = [8, 9];
+            return this.Buttons.filter((btn) =>
+                DropdownButtons.includes(btn.id)
+            );
+        },
+        filterButtonsDropDownMenuLogged() {
+            const DropdownButtons = [7, 10];
+            return this.Buttons.filter((btn) =>
+                DropdownButtons.includes(btn.id)
+            );
+        },
+    },
+    methods: {
+        toggleDropdown() {
+            this.isDropdownOpen = !this.isDropdownOpen;
+        },
+        toggleDropdownNavbar(btnId) {
+            if (this.dropdownNavbarStates[btnId]) {
+                this.dropdownNavbarStates[btnId] = false;
+            } else {
+                this.dropdownNavbarStates[btnId] = true;
+            }
+        },
+        isDropdownNavbarOpen(btnId) {
+            return this.dropdownNavbarStates[btnId] === true;
+        },
+        redirect(route) {
+            this.$inertia.visit(route);
+        },
+    },
+};
+</script>
--- resources/js/Components/navbar.vue
+++ resources/js/Components/navbar.vue
@@ -12,40 +12,10 @@
                     </a>
 
                     <!-- Links do Menu (Desktop) -->
-                    <div
-                        class="hidden md:flex md:items-center md:space-x-6 ml-8"
-                    >
-                        <div v-for="btn in filterButtonsNavBar" :key="btn.id">
-                            <button
-                                @click="
-                                    btn.dropdown
-                                        ? toggleDropdownNavbar(btn.id)
-                                        : redirect(btn.route)
-                                "
-                                class="text-gray-500 hover:text-blue-600 transition duration-300"
-                            >
-                                {{ btn.button_name }}
-                            </button>
-
-                            <ul
-                                class="absolute p-5 z-10 mt-2 w-52 rounded-lg border border-slate-200 bg-white shadow-lg text-center text-md"
-                                v-if="
-                                    btn.dropdown && isDropdownNavbarOpen(btn.id)
-                                "
-                            >
-                                <li
-                                    class="cursor-pointer text-slate-800 hover:text-blue-600"
-                                    v-for="option in JSON.parse(
-                                        btn.dropdownOptions
-                                    )"
-                                    :key="option.id"
-                                    @click="redirect(option.route)"
-                                >
-                                    {{ option.button_name }}
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
+                    <navbarButtons
+                        :Utilizador="Utilizador"
+                        :Buttons="Buttons"
+                    ></navbarButtons>
                 </div>
 
                 <NavbarSearch></NavbarSearch>
@@ -183,18 +153,18 @@
 
 <script>
 import NavbarSearch from "./Inputs/Navbar/navbarSearch.vue";
+import navbarButtons from "./Buttons/Navbar/navbarButtons.vue";
 import NavbarSearchMobile from "./Inputs/Navbar/navbarSearchMobile.vue";
 
 export default {
     components: {
         NavbarSearch,
         NavbarSearchMobile,
+        navbarButtons,
     },
     data() {
         return {
-            isDropdownOpen: false,
             isMobileMenuOpen: false,
-            dropdownNavbarStates: {},
         };
     },
     props: {
@@ -205,43 +175,9 @@ export default {
             type: Array,
         },
     },
-    computed: {
-        filterButtonsNavBar() {
-            const navbarButtons = [1, 2, 3, 4, 5, 6];
-            return this.Buttons.filter((btn) => navbarButtons.includes(btn.id));
-        },
-        filterButtonsDropDownMenuNotLogged() {
-            const DropdownButtons = [8, 9];
-            return this.Buttons.filter((btn) =>
-                DropdownButtons.includes(btn.id)
-            );
-        },
-        filterButtonsDropDownMenuLogged() {
-            const DropdownButtons = [7, 10];
-            return this.Buttons.filter((btn) =>
-                DropdownButtons.includes(btn.id)
-            );
-        },
-    },
     methods: {
-        toggleDropdown() {
-            this.isDropdownOpen = !this.isDropdownOpen;
-        },
         toggleMobileMenu() {
             this.isMobileMenuOpen = !this.isMobileMenuOpen;
-        },
-        toggleDropdownNavbar(btnId) {
-            if (this.dropdownNavbarStates[btnId]) {
-                this.dropdownNavbarStates[btnId] = false;
-            } else {
-                this.dropdownNavbarStates[btnId] = true;
-            }
-        },
-        isDropdownNavbarOpen(btnId) {
-            return this.dropdownNavbarStates[btnId] === true;
-        },
-        redirect(route) {
-            this.$inertia.visit(route);
         },
     },
 };
