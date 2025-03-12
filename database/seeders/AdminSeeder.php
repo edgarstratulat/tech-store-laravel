14f600833bd122b67fcb35571c7f5bd7c23965d8
Edgar Sousa <stratulatedgar@gmail.com>
2025-03-12 15:52:49 +0000

    chore: Add AdminSeeder to create user id 1 automatically

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = [
+            [
+                'name' => 'Edgar Sousa',
+                'email' => 'edgar@admin.pt',
+                'password' => bcrypt('123'),
+            ]
+        ];
+
+        DB::table('Users')->insert($user);
+    }
+}
