f3eb930023c7f185cf1f9343c35b0b0ffefd6be2
Edgar Sousa <stratulatedgar@gmail.com>
2025-03-20 09:50:57 +0000

    chore: Delete and update product table.

--- app/Http/Controllers/Admin/Products/DeleteProductController.php
+++ app/Http/Controllers/Admin/Products/DeleteProductController.php
@@ -5,9 +5,7 @@ namespace App\Http\Controllers\Admin\Products;
 use App\Http\Controllers\Controller;
 use Inertia\Inertia;
 use App\Models\AdminButton;
-use App\Models\Category;
 use App\Models\Product;
-use App\Models\subCategory;
 use Illuminate\Http\Request;
 
 class DeleteProductController extends Controller
@@ -15,19 +13,16 @@ class DeleteProductController extends Controller
     public function showDeleteForm()
     {
         $button = AdminButton::all();
-        $products = Product::select('id','name')->get();
-        $categories = Category::all();        
+        $products = Product::select('id','name')->get();     
         
         return Inertia::render('Admin/Products/deleteProduto', [
             'adminBtn' => $button,
             'products' => $products,
-            'categories' => $categories,
         ]);
     }
 
     public function DeleteProducts(Request $request)
     {
-
 
         $products = Product::find($request->id);
         
@@ -35,7 +30,6 @@ class DeleteProductController extends Controller
             $products->delete();
         }
         
-
         return Inertia::location('/dashboard');
     }
 }
--- app/Http/Controllers/Admin/Products/UpdateProductController.php
+++ app/Http/Controllers/Admin/Products/UpdateProductController.php
@@ -3,135 +3,37 @@
 namespace App\Http\Controllers\Admin\Products;
 
 use App\Http\Controllers\Controller;
-use App\Models\Acessorio;
 use Inertia\Inertia;
 use App\Models\AdminButton;
-use App\Models\Telemovel;
-use App\Models\ComponentePC;
-use App\Models\Computer;
-use App\Models\Periferico;
-use App\Models\Promocoes;
+use App\Models\Product;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class UpdateProductController extends Controller
 {
     public function showUpdateProductsForm()
     {
         $buttons = AdminButton::all();
-
-        $telemovel = Telemovel::select('id', 'name', 'price', 'stock')->get();
-        $comp = ComponentePC::select('id', 'name', 'price', 'stock')->get();
-        $peri = Periferico::select('id', 'name', 'price', 'stock')->get();
-        $pc = Computer::select('id', 'name', 'price', 'stock')->get();
-        $ace = Acessorio::select('id', 'name', 'price', 'stock')->get();
+        $product = Product::select('id', 'name', 'price', 'stock')->get();
 
         return Inertia::render('Admin/Products/updateProduto', [
             'adminBtn' => $buttons,
-             'Telemovel' => $telemovel,
-            'ComponentePC' => $comp,
-            'Periferico' => $peri,
-            'Computador' => $pc,
-            'Acessorio' => $ace
+            'product' => $product,
          ]);
     }
 
     public function updateProduct(Request $request){
         $request->validate([
-            'price' => 'numeric',
-            'stock' => 'nullable|numeric',
+            'price' => 'required|numeric',
+            'stock' => 'required|numeric',
         ]);
 
-        $tel = Telemovel::find($request->id);
-        $comp = ComponentePC::find($request->id);
-        $peri = Periferico::find($request->id);
-        $pc = Computer::find($request->id);
-        $ace = Acessorio::find($request->id);
+        $prod = Product::find($request->id);
 
-        if($tel){
-            $tel->update([
+        if($prod){
+            $prod->update([
                     'price' => $request->price,
                     'stock' => $request->stock,
             ]);
-            
-            if($tel->promocao_id){
-                $promo = Promocoes::find($tel->promocao_id);
-                if($promo){
-                $promo->update([
-                    'price' => $request->price,
-                    'stock' => $request->stock,
-                ]);
-                }  
-            }
-        }
-
-        if($comp){
-                $comp->update([
-                    'price' => $request->price,
-                    'stock' => $request->stock,
-                ]);
-            
-            if($comp->promocao_id){
-                $promo = Promocoes::find($comp->promocao_id);
-                if($promo){
-                    $promo->update([
-                        'price' => $request->price,
-                        'stock' => $request->stock,
-                    ]);
-                }  
-            }
-        }
-
-        if($peri){
-            $peri->update([
-                'price' => $request->price,
-                'stock' => $request->stock,
-            ]);
-
-            if($peri->promocao_id){
-                $promo = Promocoes::find($peri->promocao_id);
-                if($promo){
-                    $promo->update([
-                        'price' => $request->price,
-                        'stock' => $request->stock,
-                    ]);
-                } 
-            }
-        }
-
-        if($pc){
-            $pc->update([
-                'price' => $request->price,
-                'stock' => $request->stock,
-            ]);
-
-            if($pc->promocao_id){
-                $promo = Promocoes::find($pc->promocao_id);
-                if($promo){
-                    $promo->update([
-                        'price' => $request->price,
-                        'stock' => $request->stock,
-                    ]);
-                }
-            }
-        }
-
-        if($ace){
-            $ace->update([
-                'price' => $request->price,
-                'stock' => $request->stock,
-            ]);
-
-            if($ace->promocao_id){
-                $promo = Promocoes::find($ace->promocao_id);
-                if($promo){
-                    $promo->update([
-                        'price' => $request->price,
-                        'stock' => $request->stock,
-                    ]);
-                }
-            }
-
         }
 
         return Inertia::location('/dashboard');
