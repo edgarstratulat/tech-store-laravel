257422fdb0b3a446b87cb6bced6a68b00cdec8bd
Edgar Sousa <stratulatedgar@gmail.com>
2025-03-24 17:58:33 +0000

    feat: Search products in mobile devices.

--- resources/js/Components/Inputs/Navbar/navbarSearchMobile.vue
+++ resources/js/Components/Inputs/Navbar/navbarSearchMobile.vue
@@ -1,8 +1,21 @@
+<script setup>
+import { ref } from "vue";
+import { router } from "@inertiajs/vue3";
+
+const query = ref("");
+
+const search = () => {
+    router.get("/search", { query: query.value });
+};
+</script>
+
 <template>
     <input
         type="text"
-        placeholder="Pesquisar..."
-        class="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-900 focus:border-transparent"
+        v-model="query"
+        placeholder="O que procuras?"
+        @keyup.enter="search"
+        class="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
     />
 </template>
 
