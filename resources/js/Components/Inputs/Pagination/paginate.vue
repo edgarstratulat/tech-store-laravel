228bddfbdb65d15b0fba88c0454fb95229c15947
Edgar Sousa <stratulatedgar@gmail.com>
2025-03-24 15:09:27 +0000

    style: Added color, hover active page in pagination navbar.

--- resources/js/Components/Inputs/Pagination/paginate.vue
+++ resources/js/Components/Inputs/Pagination/paginate.vue
@@ -4,9 +4,9 @@
             <Link
                 v-if="link.url"
                 :href="link.url"
-                class="px-4 py-2 text-sm font-medium bg-white border border-gray-300 rounded-sm"
+                class="px-4 py-2 text-sm font-medium bg-white border border-gray-300 rounded-sm hover:bg-gray-100"
                 :class="{
-                    'bg-blue-500 text-white ': link.active,
+                    'bg-blue-400 text-white hover:bg-blue-400 ': link.active,
                 }"
                 v-html="link.label"
             />
