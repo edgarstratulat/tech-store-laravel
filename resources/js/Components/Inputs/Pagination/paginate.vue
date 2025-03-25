9e87043cab1dcf0bb4b32d2abad7e7f49b2f7a58
Edgar Sousa <stratulatedgar@gmail.com>
2025-03-25 16:59:32 +0000

    fix (i think) : No color pagination

--- resources/js/Components/Inputs/Pagination/paginate.vue
+++ resources/js/Components/Inputs/Pagination/paginate.vue
@@ -4,9 +4,9 @@
             <Link
                 v-if="link.url"
                 :href="link.url"
-                class="px-4 py-2 text-sm text-black font-medium bg-white border border-gray-300 rounded-sm hover:bg-gray-100"
+                class="px-4 py-2 text-sm text-black font-medium bg-white border border-gray-300 rounded-sm"
                 :class="{
-                    'bg-blue-400 text-white hover:bg-blue-500 ': link.active,
+                    'bg-blue-700 text-white hover:bg-blue-500 ': link.active,
                 }"
                 v-html="link.label"
             />
