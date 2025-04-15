1af14cc2172247b8f058310a890bd86050096d09
Edgar Sousa <stratulatedgar@gmail.com>
2025-04-15 09:44:40 +0100

    style: Change language component added some style.

--- resources/js/Components/Inputs/language/changeLang.vue
+++ resources/js/Components/Inputs/language/changeLang.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="locale-changer">
-        <select v-model="$i18n.locale">
+        <select
+            v-model="$i18n.locale"
+            class="p-2 rounded-md border border-gray-300 bg-white text-gray-700 shadow-sm hover:bg-gray-50 cursor-pointer"
+        >
             <option
                 v-for="locale in $i18n.availableLocales"
                 :key="`locale-${locale}`"
