47ba88396836a8e5b8f2d5a6f84c009172b53469
Edgar Sousa <stratulatedgar@gmail.com>
2025-04-16 18:05:25 +0100

    chore: Order by price and sale_price > 0. Just 8 showing in home page.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends Controller
 {
     public function showProductsHome()
     {
-        $products = Product::with('category')->with('subcategory')->select('id', 'name', 'slug', 'price', 'sku', 'sale_price', 'description', 'category_id', 'subcategory_id', 'image_path', 'stock')->orderBy('price', 'desc')->where('stock', '>', 0)->take(9)->get();
+        $products = Product::with('category')->with('subcategory')->select('id', 'name', 'slug', 'price', 'sku', 'sale_price', 'description', 'category_id', 'subcategory_id', 'image_path', 'stock')->orderBy('price', 'desc')->where('sale_price', '>', 0)->where('stock', '>', 1)->take(8)->get();
 
         $computers = Product::with('category')->with('subcategory')->select('id', 'name', 'slug', 'price', 'sale_price', 'description', 'category_id', 'subcategory_id', 'image_path', 'stock')->where('category_id', 1)->orderBy('id', 'asc')->take(4)->get();
 
