911288d8a9ed22c519a3f72abd500c8b2f37b69b
Edgar Sousa <stratulatedgar@gmail.com>
2025-04-16 17:49:45 +0100

    style: Added flags to change language component instead of text.

--- resources/js/Components/Inputs/language/changeLang.vue
+++ resources/js/Components/Inputs/language/changeLang.vue
@@ -2,15 +2,22 @@
     <div class="locale-changer">
         <select
             v-model="$i18n.locale"
-            class="p-2 rounded-md border border-gray-300 bg-white text-gray-700 shadow-sm hover:bg-gray-50 cursor-pointer"
+            class="p-2 rounded-md border border-gray-300 bg-white text-gray-700 shadow-sm hover:bg-gray-50 cursor-pointer appearance-none"
         >
             <option
-                v-for="locale in $i18n.availableLocales"
-                :key="`locale-${locale}`"
+                v-for="(flag, locale) in localeFlags"
+                :key="locale"
                 :value="locale"
             >
-                {{ locale }}
+                {{ flag }}
             </option>
         </select>
     </div>
 </template>
+
+<script setup>
+const localeFlags = {
+    pt: "🇵🇹",
+    en: "🇬🇧",
+};
+</script>
